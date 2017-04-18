be6b367bb08babf1b51794843eb43376690c474f
Marissa <Rissa@Marissas-MacBook-Pro.local>
2017-04-18 11:29:00 -0400

    This is my first commit

---
+++ python101.py
@@ -0,0 +1,9 @@
+# print "Hello, World!"
+# Print two different things on the same line
+print ("Hello, World", "Again")
+
+print """This
+will ignore
+the new lines
+until it sees
+another 3 double quotes"""
